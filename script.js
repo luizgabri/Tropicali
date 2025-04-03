f26bbb191391c18cb8525eba1186b337dc51035f
luizgabri <160794599+luizgabri@users.noreply.github.com>
2025-04-03 19:30:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-document.addEventListener('DOMContentLoaded', function(){
+ddocument.addEventListener('DOMContentLoaded',function(){
 
- const botaoDEAcessibilidade=document.getElementById('botao-acessibilidade');
+    const botaoDeAcessibilidade=document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessbilidade=document.getElementById('opcoes-acessibilidade');
+    
+    botaoDeAcessibilidade.addEventListener('click',function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessbilidade.classList.toggle('apresenta-lista');
+    
+    const botaoSelecionado=botaoDeAcessibilidade.getAttribute('aria-expanded')=== 'true';
 
-}
-)
+    botaoDeAcessibilidade.setAttribute('aria-expanded',!botaoSelecionado)
+
+    })
+    
+    
+    const aumentaFontedoBotao = document.getElementsById('aumentar-fonte')
+    const diminuiFontedoBotao = document.getElementsById('diminuir-fonte')
+
+    const alternaContraste = document.getElementsById('alterna-fonte')
+    })
