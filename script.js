ecbf2b6ee56b44b9e822b888b7ef0527f6a7926e
luizgabri <160794599+luizgabri@users.noreply.github.com>
2025-04-10 20:20:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ ddocument.addEventListener('DOMContentLoaded',function(){
     
     botaoDeAcessibilidade.addEventListener('click',function(){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessbilidade.classList.toggle('apresenta-lista');
+        opcoesDeAcessbilidade.classList.toggle('apresenta-)lista');
     
     const botaoSelecionado=botaoDeAcessibilidade.getAttribute('aria-expanded')=== 'true';
 
@@ -13,9 +13,29 @@ ddocument.addEventListener('DOMContentLoaded',function(){
 
     })
     
-    
     const aumentaFontedoBotao = document.getElementsById('aumentar-fonte')
     const diminuiFontedoBotao = document.getElementsById('diminuir-fonte')
 
     const alternaContraste = document.getElementsById('alterna-fonte')
+
+    letTamnhoAtualFont = 1;
+
+    aumentaFonteBotao.addEventListener ('clik', function(){
+      tananhoAtualFont += 0.1;
+      document.body.style.fontSize = `${tamanhoAtualFont}rem`
     })
+
+    aumentaFonteBotao.addEventListener ('clik', function(){
+        tananhoAtualFont += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFont}rem`
+      })
+
+    alternaContraste.addEventListener('clik', function(){
+        document.body.classList.toggle('altoContraste')
+    })
+})
+
+ScrollReveal().reveal('#inicio',{delay: 500});
+ ScrollReveal().reveal('#tropicalia',{delay: 500});
+ ScrollRevea().reveal('#galeria',{delay: 500});
+ ScrollReveal().reveal('#contato',{delay: 500});
